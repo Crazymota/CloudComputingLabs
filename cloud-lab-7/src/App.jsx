f28e04efdb121f19b7ea83999c9681e85bbdf7b3
Crazymota <162179868+Crazymota@users.noreply.github.com>
2024-12-10 12:44:01 -0500

    Update App.jsx

--- cloud-lab-7/src/App.jsx
+++ cloud-lab-7/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <>
 
-      <h1>Vite + React Ryan</h1>
+      <h1>Vite + React Kevin</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
